7c470346b13b7906ab1d002a8c442375bc1dc83f
Quojo_Steve <quojosteve@gmail.com>
2023-07-28 14:53:16 +0000

    Almost done

--- js/loadmore.js
+++ js/loadmore.js
@@ -3,15 +3,20 @@ const loadmore = document.querySelector('.btn-load-more');
 let currentItems = 4
 loadmore.addEventListener('click', (e) => {
     const elementList = [...document.querySelectorAll('.row .imagess')];
+    loadmore.style.background = "white"
+    loadmore.style.color = "white"
     e.target.classList.add('show-loader');
+    
+
 
     for (let i = currentItems; i < currentItems+4; i++) {
         setTimeout(function () {
             e.target.classList.remove('show-loader');
+            loadmore.style.background = "dodgerblue"
+            loadmore.style.color = "#1bd8d8"
             if (elementList[i]) {
                 console.log(elementList[i])
                 elementList[i].style.display = "block";
-
             }
         }, 3000)        
     }
@@ -20,4 +25,5 @@ loadmore.addEventListener('click', (e) => {
     if (currentItems >= elementList.length) {
         event.target.classList.add('loaded')
     }
+    
 })
